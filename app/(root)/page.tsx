15cdebdc0b4ba58ccec0b0f93f218ff2b558d20c
Ayan <ayan2572006@gmail.com>
2024-07-10 00:05:00 +0530

    fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,6 @@ import { currentUser } from "@clerk/nextjs/server";
 async function Home() {
   const user = await currentUser();
   if (!user) return null;
-
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
