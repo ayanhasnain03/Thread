dfca2a7b76c902bc980d625052dda0ea3ed588e1
Ayan <ayan2572006@gmail.com>
2024-07-09 16:15:09 +0530

    fix

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -41,32 +41,28 @@ const ThreadCard = ({
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
-            <Link href={`/profile/${author.id}`}>
-              <a className="relative h-11 w-11">
-                <Image
-                  src={author.image}
-                  alt="user_community_image"
-                  width={44}
-                  height={44}
-                  className="cursor-pointer rounded-full"
-                />
-              </a>
+            <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+              <Image
+                src={author.image}
+                alt="user_community_image"
+                fill
+                className="cursor-pointer rounded-full"
+              />
             </Link>
+
             <div className="thread-card_bar" />
           </div>
 
           <div className="flex w-full flex-col">
-            <Link href={`/profile/${author.id}`}>
-              <a className="w-fit">
-                <h4 className="cursor-pointer text-base-semibold text-light-1">
-                  {author.name}
-                </h4>
-              </a>
+            <Link href={`/profile/${author.id}`} className="w-fit">
+              <h4 className="cursor-pointer text-base-semibold text-light-1">
+                {author.name}
+              </h4>
             </Link>
             <p className="text-small-regular text-light-2">{content}</p>
 
-            <div className="mt-5 flex gap-3">
-              <div className="flex gap-3.5">
+            <div className="mt-5 flex  gap-3">
+              <div className="flex  gap-3.5">
                 <Image
                   src="/assets/heart-gray.svg"
                   alt="heart"
@@ -75,32 +71,30 @@ const ThreadCard = ({
                   className="cursor-pointer"
                 />
               </div>
-              <div className="flex gap-3.5">
+              <div className="flex  gap-3.5">
                 <Link href={`/thread/${id}`}>
-                  <a>
-                    <Image
-                      src="/assets/reply.svg"
-                      alt="reply"
-                      width={24}
-                      height={24}
-                      className="cursor-pointer"
-                    />
-                  </a>
+                  <Image
+                    src="/assets/reply.svg"
+                    alt="heart"
+                    width={24}
+                    height={24}
+                    className="cursor-pointer"
+                  />
                 </Link>
               </div>
-              <div className="flex gap-3.5">
+              <div className="flex  gap-3.5">
                 <Image
                   src="/assets/repost.svg"
-                  alt="repost"
+                  alt="heart"
                   width={24}
                   height={24}
                   className="cursor-pointer"
                 />
               </div>
-              <div className="flex gap-3.5">
+              <div className="flex  gap-3.5">
                 <Image
                   src="/assets/share.svg"
-                  alt="share"
+                  alt="heart"
                   width={24}
                   height={24}
                   className="cursor-pointer"
@@ -109,9 +103,9 @@ const ThreadCard = ({
 
               {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
-                  <a className="mt-1 text-subtle-medium text-gray-1">
+                  <p className="mt-1 text-subtle-medium text-gray-1">
                     {comments.length} replies
-                  </a>
+                  </p>
                 </Link>
               )}
             </div>
@@ -121,5 +115,4 @@ const ThreadCard = ({
     </article>
   );
 };
-
 export default ThreadCard;
