c9908c9ee78473a5fdde8ae1bf772d88e1aa0f9d
Ayan <ayan2572006@gmail.com>
2024-07-09 16:42:27 +0530

    fix it

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,4 @@
-import ThreadCard from "@/components/cards/ThreadCard";
-import { fetchPosts } from "@/lib/actions/thread.actions";
-import { currentUser } from "@clerk/nextjs/server";
-export default async function Home() {
-  return (
-    <>
-      <h1 className="head-text text-left">Home</h1>
-
-      {/* <section className="mt-9 flex flex-col gap-10">
-        {result.posts.length === 0 ? (
-          <p className="np-result">No Thread Found</p>
-        ) : (
-          result.posts.map((post) => (
-            <ThreadCard
-              key={post._id}
-              id={post._id}
-              currentUserId={user?.id || ""}
-              parentId={post.parentId}
-              content={post.text}
-              author={post.author}
-              community={post.community}
-              createdAt={post.createdAt}
-              comments={post.children}
-            />
-          ))
-        )}
-      </section> */}
-    </>
-  );
-}
+const Home = () => {
+  return <div>Home</div>;
+};
+export default Home;
