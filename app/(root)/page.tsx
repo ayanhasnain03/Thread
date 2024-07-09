65e8d833ee314b94feba430d030082d8ef720e2a
Ayan <ayan2572006@gmail.com>
2024-07-09 16:41:19 +0530

    ry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,6 @@ import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs/server";
 export default async function Home() {
-  const result = await fetchPosts(1, 30);
-  const user = await currentUser();
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
