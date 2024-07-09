23cb9770a354f41877f1fd134532dc560ee61d36
Ayan <ayan2572006@gmail.com>
2024-07-09 16:21:36 +0530

    fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,25 @@
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs/server";
-export default async function Home() {
-  const result = await fetchPosts(1, 30);
-  const user = await currentUser();
-  return (
-    <>
-      <h1 className="head-text text-left">Home</h1>
 
-      <section className="mt-9 flex flex-col gap-10">
-        {result.posts.length === 0 ? (
+export default async function Home() {
+  try {
+    const [posts, user] = await Promise.all([fetchPosts(1, 30), currentUser()]);
+
+    if (!posts || !posts.posts || posts.posts.length === 0) {
+      return (
+        <>
+          <h1 className="head-text text-left">Home</h1>
           <p className="np-result">No Thread Found</p>
-        ) : (
-          result.posts.map((post) => (
+        </>
+      );
+    }
+
+    return (
+      <>
+        <h1 className="head-text text-left">Home</h1>
+        <section className="mt-9 flex flex-col gap-10">
+          {posts.posts.map((post) => (
             <ThreadCard
               key={post._id}
               id={post._id}
@@ -24,9 +31,19 @@ export default async function Home() {
               createdAt={post.createdAt}
               comments={post.children}
             />
-          ))
-        )}
-      </section>
-    </>
-  );
+          ))}
+        </section>
+      </>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return (
+      <>
+        <h1 className="head-text text-left">Home</h1>
+        <p className="np-result">
+          Error fetching threads. Please try again later.
+        </p>
+      </>
+    );
+  }
 }
